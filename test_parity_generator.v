35d6923a473d0943213b1c086f508c7278e5f4d4
Patrycja <patmarc95@gmail.com>
2017-03-05 15:26:55 +0100

    Testbench for parity_gen

--- test_parity_generator.v
+++ test_parity_generator.v
@@ -11,16 +11,17 @@
 module test_parity_generator;
 
 	// Inputs
-	reg [7:0] 		sw_i;
+	reg [7:0] 	sw_i;
 	// Outputs
-	wire [7:0] 		led_o;
-	wire [3:0] 		led_an_o;
+	wire [7:0] 	led_o;
+	wire [3:0] 	led_an_o;
 
 	// Instantiate the Unit Under Test (UUT)
 	parity_generator uut 
-	  (.sw_i(sw_i), 
+	       (.sw_i(sw_i), 
 		.led_o(led_o), 
 		.led_an_o(led_an_o));
+	
 	integer random_value ;
 	initial begin
 		sw_i = 8'b11000000;
@@ -29,7 +30,7 @@ module test_parity_generator;
 			random_value = $random%32;
 			sw_i = random_value;
 			#200;
-	end
+		end
 	end
       
 endmodule
