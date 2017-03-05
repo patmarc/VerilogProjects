951c941189238af846dd7df085679241a9da8859
Patrycja <patmarc95@gmail.com>
2017-03-05 15:24:35 +0100

    Parity gen. update

--- parity_generator.v
+++ parity_generator.v
@@ -9,25 +9,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module parity_generator
-<<<<<<< HEAD
- #(parameter N_SW = 8,							// number of switches / bits in word
-=======
-      #(parameter N_SW = 8,							// number of switches / bits in word
->>>>>>> origin/master
-	parameter N_LED = 8,							// number of segments at display
-	parameter N_LED_AN = 4,							// number of displays
+      #(parameter N_SW = 8,								// number of switches / bits in word
+	parameter N_LED = 8,								// number of segments at display
+	parameter N_LED_AN = 4,								// number of displays
 	  
-	parameter E = 8'b00110001,					// even
-	parameter O = 8'b00000011)					// odd
-	  
-<<<<<<< HEAD
-  (input wire [N_SW-1:0] 			sw_i,
+	parameter E = 8'b00110001,							// even
+	parameter O = 8'b00000011)							// odd
+
+       (input wire [N_SW-1:0] 			sw_i,
 	output reg [N_LED-1:0] 			led_o,
-=======
-       (input wire [N_SW-1:0] 		sw_i,
-	output reg [N_LED-1:0] 		led_o,
->>>>>>> origin/master
-	output wire [N_LED_AN-1:0] 	led_an_o);
+	output wire [N_LED_AN-1:0] 		led_an_o);
 	  
 	  
 	reg xor_value;
@@ -39,14 +30,8 @@ module parity_generator
 			assign led_o = O;
 		end
 		else begin
-<<<<<<< HEAD
 			assign led_o = E;
-		end
-			
-=======
-			assign led_o = O;
 		end		
->>>>>>> origin/master
 	end
 
 endmodule // parity_generator
