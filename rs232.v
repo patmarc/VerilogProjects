2e940afba9b617afa151d2e216ccd455bad5c57e
Patrycja <patmarc95@gmail.com>
2017-03-07 23:12:59 +0100

    Receiver rs232
    to do : transmitter

--- rs232.v
+++ rs232.v
@@ -5,38 +5,52 @@
 // Design Name: 
 // Module Name:    	rs232 
 //
-// To do : 				add parity bit, N_BIT_STOP , rst
+// 
 //////////////////////////////////////////////////////////////////////////////////
-module rs232
-      #(parameter 	N_BIT_STOP = 1)									// number of stop bits 1 or 2 	
- 
-       (input wire 		clk_in,
-	input wire 		rst_in,
+module rs232	
+  (input wire 			clk_in,
+	input wire 			rst_in,
 	input wire  		rxd_in,
-	input wire		start_flag,
 	output reg [7:0] 	txd_out,
-	output reg		end_flag);
 	
-	reg [5:0] 		bit_nr;
+	// receiver
+	input wire			ctrl_rdata,									
+	output reg			ctrl_rready,
+	
+	//transmitter
+	input wire			ctrl_wdata,									
+	output wire			ctrl_wready);
+	
+	reg [5:0] 	bit_nr;
 	reg			bit_start;
 	reg			bit_stop;
+	reg [7:0]	txd_temp;
 	
-	initial @ (negedge start_flag) begin
+	// receiver
+	initial @ (negedge ctrl_rdata) begin : receive_begin
 		bit_start = rxd_in;
 		bit_nr = 5'b00001;
-		end_flag = 0;
+		ctrl_rready = 0;
 	end
 	
-	always @ (negedge clk_in or posedge clk_in) begin
-		if(end_flag == 0) begin
+	always @ (negedge clk_in or posedge clk_in) begin : receive_main_block
+		if(ctrl_rready == 0) begin
 			if(bit_nr < 5'd9) begin
-				txd_out [bit_nr-1] = rxd_in;
+				txd_temp [bit_nr-1] = rxd_in;
 				bit_nr = bit_nr +1;
 			end
 			else begin
 				bit_stop = rxd_in;
-				assign end_flag = 1;
+				assign ctrl_rready = 1;
 			end
 		end
 	end
+	
+	initial @ (posedge ctrl_rready) begin	: receive_end
+		assign txd_out = txd_temp;
+	end
+	
+	//transmitter
+	
+	
 endmodule // rs232
