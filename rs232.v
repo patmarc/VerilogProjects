79c877c5a9439ce31c2344a635a00b2cb41cf5ea
Patrycja <patmarc95@gmail.com>
2017-03-08 22:26:09 +0100

    receiver and transmitter rs232
    to do : separate receiver and transmitter

--- rs232.v
+++ rs232.v
@@ -1,56 +1,89 @@
 `timescale 1s / 1ms
 //////////////////////////////////////////////////////////////////////////////////
 //
-// Create Date:    	20:38:44 03/06/2017 
-// Design Name: 
-// Module Name:    	rs232 
-//
+// Design Name: 		rs232
+// Description:    	Port rs232 ( receiver and transmitter)
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module rs232	
+module rs232
   (input wire 			clk_in,
-	input wire 			rst_in,
 	input wire  		rxd_in,
-	output reg [7:0] 	txd_out,
-	
-	// receiver
-	input wire			ctrl_rdata,									
-	output reg			ctrl_rready,
-	
-	//transmitter
-	input wire			ctrl_wdata,									
-	output wire			ctrl_wready);
+	output reg			txd_out);
 	
 	reg [5:0] 	bit_nr;
 	reg			bit_start;
 	reg			bit_stop;
 	reg [7:0]	txd_temp;
+	reg [7:0]	txd_sum;
+	
+	reg			ctrl_rready;
+	reg			ctrl_tready;
+	reg			ctrl_wdata;
 	
 	// receiver
-	initial @ (negedge ctrl_rdata) begin : receive_begin
-		bit_start = rxd_in;
-		bit_nr = 5'b00001;
-		ctrl_rready = 0;
+	initial begin
+		ctrl_rready = 1;
+	end
+	always @ (negedge rxd_in) begin : r_begin
+		if(ctrl_rready == 1)	begin
+			bit_nr = 0;
+			ctrl_rready = 0;
+			ctrl_wdata = 1;
+		end
 	end
 	
-	always @ (negedge clk_in or posedge clk_in) begin : receive_main_block
-		if(ctrl_rready == 0) begin
-			if(bit_nr < 5'd9) begin
-				txd_temp [bit_nr-1] = rxd_in;
-				bit_nr = bit_nr +1;
+	always @ (negedge clk_in or posedge clk_in) begin : r_main_block
+		if(ctrl_rready == 0 ) begin
+			if(bit_nr == 0) begin
+				bit_start = rxd_in;
+				bit_nr = bit_nr + 1;
 			end
 			else begin
-				bit_stop = rxd_in;
-				assign ctrl_rready = 1;
+				if(bit_nr < 5'd9) begin
+					txd_temp [bit_nr-1] = rxd_in;
+					bit_nr = bit_nr +1;
+				end
+				else begin
+					bit_stop = rxd_in;
+					ctrl_rready = 1;
+					ctrl_wdata = 0;
+				end
 			end
 		end
 	end
 	
-	initial @ (posedge ctrl_rready) begin	: receive_end
-		assign txd_out = txd_temp;
+	always @ (negedge ctrl_wdata) begin	: r_end
+		assign txd_sum = txd_temp + 8'h20;								//sum
+		
 	end
+	
 	
 	//transmitter
 	
+	always @ (negedge ctrl_wdata) begin : t_begin
+		ctrl_tready = 0;
+		bit_nr = 0;
+	end
+	
+	always @ (negedge clk_in or posedge clk_in) begin : t_main_block
+		if(ctrl_tready == 0) begin
+			if( bit_nr == 0) begin
+				txd_out = 0;
+				bit_nr = bit_nr +1;
+			end
+			else begin
+				if(bit_nr < 5'd9) begin
+					txd_out = txd_sum [bit_nr-1]; 
+					bit_nr = bit_nr + 1;
+				end
+				else begin
+					txd_out = 1;
+					bit_nr = bit_nr + 1;
+					ctrl_tready = 1;
+				end
+			end
+		end
+		
+	end
 	
 endmodule // rs232
