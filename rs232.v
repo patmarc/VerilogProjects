6520d0a13c373ff9f2aaee7751b690c641dce398
Patrycja Marchewka <patmarc95@gmail.com>
2017-03-21 23:16:39 +0100

    rs232 with reset
    Reset isn`t working.

--- rs232.v
+++ rs232.v
@@ -1,7 +1,7 @@
 `timescale 1s / 1ms
 //////////////////////////////////////////////////////////////////////////////////
 //
-// Design Name: 		rs232
+// Design Name: 	rs232
 // Description:    	Port rs232 ( receiver and transmitter)
 // 
 //////////////////////////////////////////////////////////////////////////////////
@@ -19,60 +19,72 @@ module rs232
 	
 	reg		ctrl_rready;
 	reg		ctrl_tready;
+	reg		ctrl_wdata;
 	reg		ctrl_rstart;
-
+	
+	// receiver
 	initial begin
 		ctrl_rready = 0;
 		ctrl_tready = 1;
 		ctrl_rstart = 0;
 	end
 	
-	always @ (posedge clk_in ) begin 
-	//receiver
-		if(ctrl_rready == 0) begin
-			if(rxd_in == 0 & ctrl_rstart != 1) begin
-				bit_nr = 0;
-				ctrl_rstart = 1;
-				txd_out = 1;
+	always @ (posedge clk_in or posedge rst_in ) begin
+	
+		if (rst_in == 1) begin
+			ctrl_rready = 0;
+			ctrl_tready = 1;
+			ctrl_rstart = 0;
+			//txd_out = 1;
+			bit_nr =0;
+		end
+		else begin
+		//receiver
+			if(ctrl_rready == 0 ) begin //& ctrl_rst == 0
+				if(rxd_in == 0 & ctrl_rstart != 1) begin
+					bit_nr = 0;
+					ctrl_rstart = 1;
+					txd_out = 1;
+				end
+				if (ctrl_rstart == 1) begin
+					if(bit_nr ==0) begin
+						bit_start = rxd_in;
+						bit_nr = bit_nr + 1;
+					end
+					else begin
+						if(bit_nr < 5'd9) begin
+							txd_temp [bit_nr-1] = rxd_in; // serial to parallel
+							bit_nr = bit_nr +1;
+						end
+						else begin
+							bit_stop = rxd_in;
+							ctrl_rstart = 0;
+							ctrl_rready = 1;
+							txd_sum = txd_temp ;//+ 8'h20;
+							bit_nr = 0;
+							ctrl_tready = 0;
+						end
+					end
+				end
 			end
-			if (ctrl_rstart == 1) begin
-				if(bit_nr ==0) begin
-					bit_start = rxd_in;
-					bit_nr = bit_nr + 1;
+		
+			//transmitter
+			if(ctrl_tready == 0 )begin //& ctrl_rst == 0
+				if(bit_nr == 0) begin
+					bit_nr = 1;
+					txd_out = 0;
 				end
 				else begin
 					if(bit_nr < 5'd9) begin
-						txd_temp [bit_nr-1] = rxd_in; // serial to parallel
-						bit_nr = bit_nr +1;
+						txd_out = txd_sum [bit_nr-1]; // parallel to serial
+						bit_nr = bit_nr + 1;
 					end
 					else begin
-						bit_stop = rxd_in;
-						ctrl_rstart = 0;
-						ctrl_rready = 1;
-						txd_sum = txd_temp;
-						bit_nr = 0;
-						ctrl_tready = 0;
+						txd_out = 1;
+						bit_nr = bit_nr + 1;
+						ctrl_tready = 1;
+						ctrl_rready = 0;
 					end
-				end
-			end
-		end
-		
-		//transmitter
-		if(ctrl_tready == 0)begin
-			if(bit_nr == 0) begin
-				bit_nr = 1;
-				txd_out = 0;
-			end
-			else begin
-				if(bit_nr < 5'd9) begin
-					txd_out = txd_sum [bit_nr-1]; 
-					bit_nr = bit_nr + 1;
-				end
-				else begin
-					txd_out = 1;
-					bit_nr = bit_nr + 1;
-					ctrl_tready = 1;
-					ctrl_rready = 0;
 				end
 			end
 		end
