5ad62d37793a78be2998d47aa9538c511ccf2ca7
Patrycja Marchewka <patmarc95@gmail.com>
2017-05-24 13:25:00 +0200

    Update RTClock.v

--- RTC/RTClock.v
+++ RTC/RTClock.v
@@ -1,11 +1,10 @@
 `timescale 1ns / 1ps
-
-module RTClock
-  (input 			clk_i,
+module RTC
+       (input 		clk_i,
 	output [3:0] 	led_an,
-	output [6:0] 	led_seg);
+	output [6:0] 	led_seg,
+	output  	dp);
 	
-	reg [25:0] 	freq_clk = 10000000; // 10 MHz Clock 
 	reg [25:0] 	counter = 0;
 	
 	reg [3:0] minutes_1 	= 0;
@@ -17,7 +16,7 @@ module RTClock
 	
 	
 	always @ (posedge clk_i) begin
-		if(counter == freq_clk) begin
+		if(counter == 50000000) begin // 50 MHz
 			counter = 0;
 			seconds_1 = seconds_1 + 1;
 		end
@@ -28,11 +27,11 @@ module RTClock
 			seconds_1 = 0;
 			seconds_10 = seconds_10 + 1;
 		end
-		if(seconds_10  == 6) begin
+		if(seconds_10 == 6) begin
 			seconds_10 = 0;
 			minutes_1 = minutes_1 + 1;
 		end
-		if(minutes_1  == 10) begin
+		if(minutes_1 == 10) begin
 			minutes_1 = 0;
 			minutes_10 = minutes_10 + 1;
 		end
@@ -40,16 +39,26 @@ module RTClock
 			minutes_10 = 0;
 			hours_1 = hours_1 + 1;
 		end
-			
+		if(hours_1 == 10) begin
+			hours_1 = 0;
+			hours_10 = hours_10 + 1;
+		end
+		if(hours_10 == 2 & hours_1 == 4) begin
+			hours_10 = 0;
+			hours_1 = 0;
+		end
+		
 	end
 	display disp(
 		.clk_i(clk_i), 
-		.sign0(seconds_1), 
-		.sign1(seconds_10), 
-		.sign2(minutes_1),
-		.sign3(minutes_10),
+		.sign0(minutes_1), 
+		.sign1(minutes_10), 
+		.sign2(hours_1),
+		.sign3(hours_10),
 		.segments(led_seg),
-		.displays(led_an));
-
+		.displays(led_an),
+		.dp(dp),
+		.seconds(seconds_1));
 
 endmodule
+
